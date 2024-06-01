e28b8784bbc238b7c9861c9fb0b58b67a3a5b7a4
NeNorvalls <honeygeeh27@gmail.com>
2024-06-01 18:12:32 +0200

    adding console.log so it will monitor every action made

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 let display = document.getElementById('display');
 
-
 function appendToDisplay(value) {
     if (display.innerHTML === '0') {
         display.innerHTML = value;
     } else {
         display.innerHTML += value;
     }
+    console.log(`Value ${value} appended to display`);
 }
-
 
 function clearDisplay() {
     display.innerHTML = '0';
+    console.log('Display cleared');
 }
-
 
 function calculateResult() {
     try {
         display.innerHTML = eval(display.innerHTML);
+        console.log('Result calculated:', display.innerHTML);
     } catch {
         display.innerHTML = 'Error';
+        console.error('Error occurred during calculation');
     }
 }
-
-
